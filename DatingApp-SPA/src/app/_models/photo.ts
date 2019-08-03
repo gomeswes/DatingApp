1c5fc190b6f5603eafb1ed5c7b777bdfbcf15baf
Wesley Gomes <wesley.gomes@gmail.com>
2019-08-03 12:26:15 +0200

    Type scripts interfaces User and Photo

---
+++ DatingApp-SPA/src/app/_models/photo.ts
@@ -0,0 +1,12 @@
+export interface Photo {
+    id: number;
+    username: string;
+    konwnAs: string;
+    age: number;
+    gender: string;
+    created: Date;
+    lastActive: Date;
+    photoUrl: string;
+    city: string;
+    country: string;
+}
