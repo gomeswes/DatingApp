764f6d5c361f4c9543d382054e9cacfcfc43f097
Wesley Gomes <wesley.gomes@gmail.com>
2019-12-01 11:32:47 +0100

    SetMain photo api ability

--- DatingApp.API/Data/DatingRepository.cs
+++ DatingApp.API/Data/DatingRepository.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using DatingApp.API.Models;
 using Microsoft.EntityFrameworkCore;
@@ -45,6 +46,11 @@ namespace DatingApp.API.Data
         public async Task<bool> SaveAll()
         {
             return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<Photo> GetMainPhotoForUser(int userId)
+        {
+            return await _context.Photos.FirstOrDefaultAsync(p => p.UserId.Equals(userId) && p.IsMain);
         }
     }
 }
