b9a82ff12cfe4d668371a19fcffe896ed05fbdde
Wesley Gomes <gomes.wesley@gmail.com>
2020-02-19 22:20:01 +0100

    Action Filter for last active

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -50,6 +50,7 @@ namespace DatingApp.API
                             ValidateIssuer = false,
                             ValidateAudience = false
                         });
+            services.AddScoped<LogUserActivity>();
             
         }
 
